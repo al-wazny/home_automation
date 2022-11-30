578f4fd082ac81a541b7fbd5d95d478438352888
ali.al-wazny <ali.al-wazny@flagbit.de>
2022-11-30 18:36:44 +0100

    chore: Remove private credentials

--- network_scanner.py
+++ network_scanner.py
@@ -1,12 +1,13 @@
 from scapy.all import ARP, Ether, srp
 
-ip = "192.168.2.33/24"
+ip = "192.168.2.0/24"
 
+#create the package
 arp = ARP(pdst=ip)
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-packet = ether/arp
+package = ether/arp
 
-result = srp(packet, timeout=3, verbose=0)[0] #thanks to the specified subnet mask, all possible hosts within the network will receive the package and responde
+result = srp(package, timeout=3, verbose=0)[0] #thanks to the specified subnet mask, all possible hosts within the network will receive the package and responde
 
 for _, received_packages in result:
     print(received_packages.hwsrc) #print the mac address of all devices inside the network
